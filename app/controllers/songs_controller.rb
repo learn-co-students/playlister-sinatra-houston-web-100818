05a49b80548910a555466ec09a2eca73a4d097c4
barbaradecares <barbaradecares@hotmail.com>
2018-11-01 17:14:44 -0500

    edit form done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,13 +6,43 @@ class SongsController < ApplicationController
         erb :"songs/index"
     end
 
-    get '/songs/:slug' do
-        @song = Song.find_by_slug(params[:slug])
-        erb :"songs/show"
-    end 
-    
     get '/songs/new' do
         @songs = Song.all
         erb :"songs/new"
     end 
+
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :"songs/edit"
+    end 
+
+    patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        artist = Artist.find_or_create_by(name: params["artist"])
+        
+        @song.update(name: params["name"], artist: artist)
+
+        params["genres"].each do |genre|
+            SongGenre.create(genre_id: genre, song_id: @song.id)
+        end 
+        
+         redirect "/songs/#{@song.slug}"
+    end 
+
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :"songs/show"
+    end 
+
+    post '/songs' do
+        artist = Artist.find_or_create_by(name: params["artist"])
+        new_song = Song.create(name: params["name"], artist: artist)
+        params["genres"].each do |genre|
+            SongGenre.create(genre_id: genre, song_id: new_song.id)
+        end 
+        redirect '/songs'
+    end 
+    
+  
+    
 end
